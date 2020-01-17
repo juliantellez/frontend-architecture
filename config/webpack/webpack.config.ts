30243570ea037bdea33ab307048e216cdd675ddf
Julian <juliantellezmendez@gmail.com>
2020-01-17 00:52:52 +0000

    chore(scaffold): webpack dev server

--- config/webpack/webpack.config.ts
+++ config/webpack/webpack.config.ts
@@ -1,14 +1,19 @@
 import * as path from "path";
+import * as CopyWebpackPlugin from "copy-webpack-plugin";
+import * as HtmlWebpackPlugin from "html-webpack-plugin";
 import { Configuration } from "webpack";
 
 import jsRule from "./Rules/jsRule";
 import cssRule from "./Rules/cssRule";
 
-const PATH_SRC = path.resolve(__dirname, '..', '..', 'src', 'main');
-const PATH_BUILD = path.resolve(__dirname, '..', '..', 'dist');
+const PATH_ROOT = path.resolve(__dirname, "..", "..");
+const PATH_SRC = path.resolve(PATH_ROOT, "src", "main");
+const PATH_BUILD = path.resolve(PATH_ROOT, "dist");
 
 const createWebpackConfig = (): Configuration => {
   return {
+    mode: "development",
+    devtool: "inline-source-map",
     entry: PATH_SRC,
     output: {
       path: PATH_BUILD,
@@ -19,6 +24,24 @@ const createWebpackConfig = (): Configuration => {
     },
     module: {
       rules: [jsRule, cssRule]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(PATH_ROOT, "assets", "index.html")
+      }),
+      new CopyWebpackPlugin([
+        {
+          from: path.join(PATH_ROOT, "assets", "fonts"),
+          to: path.join(PATH_ROOT, "dist", "assets", "fonts")
+        }
+      ])
+    ],
+    devServer: {
+      compress: true,
+      contentBase: PATH_SRC,
+      hot: true,
+      port: 3000,
+      historyApiFallback: true
     }
   };
 };
